8688145baeb17eb01e99184767177a3c542ceba3
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-02-02 11:55:04 -0600

    Add files via upload

---
+++ prestamo/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prestamo
+{
+    internal class Class1
+    {
+        double p;
+
+        public Class1(){}
+
+        public Class1(double p) 
+        {
+          this.p = p;
+        }
+
+        public void tiempo() 
+        {
+            Console.WriteLine("PLAZOS");
+            Console.WriteLine("[1] 6  meses");
+            Console.WriteLine("[2] 9  meses");
+            Console.WriteLine("[3] 12 meses");
+            Console.WriteLine("[4] 18 meses");
+            Console.WriteLine("[5] 24 meses");
+            Console.WriteLine("[6] 36 meses");
+            Console.WriteLine("[7] 48 meses");
+        }
+
+        public double plazo1(double p) 
+        {
+
+            return p + (p * 0.12);
+
+        }
+        public double plazo2(double p)
+        {
+
+            return p + (p * 0.18);
+
+        }
+
+        public double plazo3(double p)
+        {
+
+            return p + (p * 0.24);
+
+        }
+
+        public double plazo4(double p)
+        {
+
+            return p + (p * 0.36);
+
+        }
+
+        public double plazo5(double p)
+        {
+
+            return p + (p * 0.48);
+
+        }
+
+        public double plazo6(double p)
+        {
+
+            return p + (p * 0.72);
+
+        }
+
+        public double plazo7(double p)
+        {
+
+            return p + (p * 0.96);
+
+        }
+        public int validar(String Mensaje)
+        {
+
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("////////////////////////////////////");
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                    Console.WriteLine("////////////////////////////////////");
+                    tiempo();
+
+                }
+            } while (v == 0);
+            return num;
+        }
+    }
+}
---
+++ prestamo/Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+using prestamo;
+
+public class prest 
+{
+    public static void Main(string [] args) 
+    {
+
+        Class1 p = new Class1();
+        double mont =0 , montt;
+        int op , s;
+        do 
+        {
+
+            do
+            {
+                Console.WriteLine("BANCO LAFISE");
+                mont = p.validar("Ingrse el monto que desea: ");
+                Console.WriteLine("//////////////////////");
+
+            }while(mont < 100 || mont > 5000);
+
+            do
+            {
+                p.tiempo();
+                op = p.validar("Ingrese la opcion a pagar: ");
+                Console.WriteLine("//////////////////////");
+                
+
+            } while (op <= 0 || op > 7);
+
+            montt = p.plazo1(mont);
+            montt = p.plazo2(mont);
+            montt = p.plazo3(mont);
+            montt = p.plazo4(mont);
+            montt = p.plazo5(mont);
+            montt = p.plazo6(mont);
+            montt = p.plazo7(mont);
+
+            switch (op) 
+            {
+              case 1: 
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: "+mont);
+                        Console.WriteLine("Plazo: 6 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: " + mont);
+                        Console.WriteLine("Plazo: 9 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: " + mont);
+                        Console.WriteLine("Plazo: 12 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+                case 4:
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: " + mont);
+                        Console.WriteLine("Plazo: 18 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+                case 5:
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: " + mont);
+                        Console.WriteLine("Plazo: 24 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+                case 6:
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: " + mont);
+                        Console.WriteLine("Plazo: 36 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+                case 7:
+                    {
+                        Console.WriteLine("-------------------------//");
+                        Console.WriteLine("Monto a prestar: " + mont);
+                        Console.WriteLine("Plazo: 48 meses");
+                        Console.WriteLine("Monto total: " + montt);
+                        Console.WriteLine("-------------------------//");
+                    }
+                    break;
+            }
+            do 
+            {
+                s = p.validar("Desea seguir? 1.si  2.No");
+
+            }while (s <= 0 || s > 2);
+           
+
+
+        } while (s==1);
+        Console.WriteLine("Buen dia");
+    }
+}
